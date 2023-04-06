dac6d43befef2574d7b7c42021665c4173551ff2
mhneh <20120075@student.hcmus.edu.vn>
2023-04-06 22:56:08 +0700

    add rules

--- share/assets/base.js
+++ share/assets/base.js
@@ -2,16 +2,6 @@
 $(document).ready(function(){
     let menuBtn = $("#menu-btn");
     const menu = $("#navbar-menu");
-    console.log(menu);
-    function showMenu(){
-        if(menu.css("display")=="block"){
-            menu.css("display","none");
-        }
-        else{
-            menu.css("display","block");
-        }
-
-    }
     menuBtn.click(function(){
         if(menu.css("display")=="block"){
             menu.css("display","none");
