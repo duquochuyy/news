7672cb23359a5605127e8492b0f3d4c7e097f22b
duquochuyy <duquochuy2k2@gmail.com>
2023-04-12 11:06:15 +0700

    update checkpw

---
+++ logged_user/scripts/reset_pw.js
@@ -0,0 +1,10 @@
+// function checkSamePW() {
+//     var inputElement = document.getElementById("inputNewPW");
+//     if (inputElement) {
+//         var stringNewPW = inputElement.value.toString();
+//         console.log(stringNewPW);
+//     }
+
+// }
+
+// $("#btnSubmit").on('click', checkSamePW());
