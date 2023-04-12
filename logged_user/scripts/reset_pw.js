6428daa4a13b455e323184982f25c28d655ed161
duquochuyy <duquochuy2k2@gmail.com>
2023-04-12 16:59:57 +0700

    reset pw with check valid

--- logged_user/scripts/reset_pw.js
+++ logged_user/scripts/reset_pw.js
@@ -1,10 +1,28 @@
-// function checkSamePW() {
-//     var inputElement = document.getElementById("inputNewPW");
-//     if (inputElement) {
-//         var stringNewPW = inputElement.value.toString();
-//         console.log(stringNewPW);
-//     }
+$(document).ready(function(){
+    $('#resetPWForm').on('submit', function(event){
+        event.preventDefault();
+        var newPW = $('#inputNewPW').val();
+        var confirmNewPW = $('#inputConfirmNewPW').val();
+        
+        if (newPW.length < 8) {
+            $('#passwordError').removeClass('d-none')
+            $('#passwordError').addClass('d-block')
+        }
+        else {
+            $('#passwordError').removeClass('d-block')
+            $('#passwordError').addClass('d-none')
 
-// }
-
-// $("#btnSubmit").on('click', checkSamePW());
+        }
+        if (newPW !== confirmNewPW) {
+            $('#confirmPasswordError').removeClass('d-none')
+            $('#confirmPasswordError').addClass('d-block')
+        }
+        else {
+            $('#confirmPasswordError').removeClass('d-block')
+            $('#confirmPasswordError').addClass('d-none')
+        }
+        if (newPW.length >= 8 && newPW === confirmNewPW) {
+            this.submit();
+        }
+    });
+});
