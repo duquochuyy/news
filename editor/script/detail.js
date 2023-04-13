969cc2a591674e8631543738ea81ca9f34407465
NguyenKongDai <kongdaittnbk@gmail.com>
2023-04-14 02:17:47 +0700

    edit_category&tag

--- editor/script/detail.js
+++ editor/script/detail.js
@@ -2,4 +2,26 @@
 $(".sidebar ul li").on('click', function() {
     $(".sidebar ul li.active").removeClass('active');
     $(this).addClass('active');
+
+    if ($(this).hasClass('content-1')) {
+        $(".content div.content-active").addClass('content-hidden');
+        $(".content div.content-active").removeClass('content-active');
+
+        $(".content div.content-1").removeClass('content-hidden');
+        $(".content div.content-1").addClass('content-active');
+    }
+    else if ($(this).hasClass('content-2')) {
+        $(".content div.content-active").addClass('content-hidden');
+        $(".content div.content-active").removeClass('content-active');
+
+        $(".content div.content-2").removeClass('content-hidden');
+        $(".content div.content-2").addClass('content-active');
+    }
+    else if ($(this).hasClass('content-3')) {
+        $(".content div.content-active").addClass('content-hidden');
+        $(".content div.content-active").removeClass('content-active');
+
+        $(".content div.content-3").removeClass('content-hidden');
+        $(".content div.content-3").addClass('content-active');
+    }
 })
